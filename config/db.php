9b6be6fd5ebd7b966cab8d0c25b4f1490dc0aa4d
aliaxander <admin@oxcpa.ru>
2017-07-12 12:12:14 +0300

    fix charset

--- config/db.php
+++ config/db.php
@@ -5,5 +5,5 @@ return [
     'dsn' => 'mysql:host=103.250.22.104;dbname=insta',
     'username' => 'insta',
     'password' => 'mPF4F6n6lAyor3KZ',
-    'charset' => 'utf8',
+    'charset' => 'utf8mb4',
 ];
